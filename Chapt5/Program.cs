0e8ce14d816956f08bb9917bb26ea5046e6591d8
EUnseok <lspk.lpg@gmail.com>
2022-06-09 04:58:57 +0900

    Chapt5

---
+++ Chapt5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Example 1
+
+string Greet(Option<string> greetee)
+    => greetee switch
+    {
+        None<string> => "Sorry Whoo?",
+        Some<string>(var name) => $"Hello {name}",
+    };
+
+// util Minimal OPtion
+interface Option<T>
+{
+}
+
+record None<T> : Option<T>;
+
+record Some<T>(T value) : Option<T>;
