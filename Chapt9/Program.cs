3773c2d6ee25ca0edb24947abe6715052df828c8
EUnseok <lspk.lpg@gmail.com>
2022-06-19 04:45:21 +0900

    Chapt 9

--- Chapt9/Program.cs
+++ Chapt9/Program.cs
@@ -1,4 +1,6 @@
-﻿using Functional.F1;
+﻿using System.Net.WebSockets;
+using Functional.F1;
+using static Functional.F1.F;
 using static System.Console;
 
 // Example 1.
@@ -22,3 +24,40 @@ names.Map(greetFormally).ForEach(WriteLine);
 // greetWith : Greeting -> (Name- > PersonalizedGreeting)
 // arrow notaion 은 right associative 를 갖고 있어  Greeting -> Name -> PersonalizedGreeting 로 표기 가능하다.
 // greet with 같은걸 curried form 이라한다.
+
+// Example 3. Apply
+var greetInformally = greet.Apply("Hey");
+names.Map(greetInformally).ForEach(WriteLine);
+
+// Example 4.
+Some(9.0).Map(Math.Sqrt).ForEach(WriteLine);
+PersonalizedGreeting GreetMethod(Greeting gr, Name name) => $"{gr}, {name}";
+
+/*Func<Name, PersonalizedGreeting> GreetWith(Greeting greeting) 
+    => GreetMethod.Apply(greeting);*/
+
+Func<Name, PersonalizedGreeting> GreetWith1(Greeting greeting) 
+    => FuncExt.Apply<Greeting, Name, PersonalizedGreeting>(GreetMethod, greeting);
+
+Func<Name, PersonalizedGreeting> GreetWith2(Greeting greeting) 
+    => new Func<Greeting, Name, PersonalizedGreeting>(GreetMethod).Apply(greeting);
+
+// Example5. Currying
+greetWith("Hello")("World");
+var greetWith2 = greet.Curry();
+var greetNostalgically = greetWith2("안녕");
+names.Map(greetNostalgically).ForEach(WriteLine);
+
+public static class FuncExt
+{
+    public static Func<T2, R> Apply<T1, T2, R>(this Func<T1, T2, R> f, T1 t1)
+        => t2 => f(t1, t2);
+
+    public static Func<T2, T3, R> Apply<T1, T2, T3, R>(this Func<T1, T2, T3, R> f, T1 t1)
+        => (t2, t3) => f(t1, t2, t3);
+    
+    public static Func<T1, Func<T2, R>> Curry<T1, T2, R>(this Func<T1, T2, R> f)
+        => t1 => t2 => f(t1, t2);
+    public static Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R> (this Func<T1, T2, T3, R> f)
+        => t1 => t2 => t3 => f(t1, t2, t3);
+}
---
+++ Chapt9/Readme.md
@@ -0,0 +1,5 @@
+﻿## Currying
+Haskell Curry 이름에서 땀
+
+`(T1, T2, .. , Tn) -> R `, 커리하면
+`T1 -> T2 -> .. -> Tn -> R `
