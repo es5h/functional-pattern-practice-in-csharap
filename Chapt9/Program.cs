c55cab204a0c4c855e56fc81c1eb142de56ba042
EunseokOh <lspk.lpg@gmail.com>
2022-06-19 03:39:41 +0900

    chapt9

---
+++ Chapt9/Program.cs
@@ -0,0 +1,24 @@
+﻿using Functional.F1;
+using static System.Console;
+
+// Example 1.
+using Name = System.String;
+using Greeting = System.String;
+using PersonalizedGreeting = System.String;
+
+var greet = (Greeting greet, Name name) => $"{greet}, {name}";
+
+Name[] names = {"Tristan", "ivan"};
+names.Map(n => greet("Hello", n)).ForEach(WriteLine);
+
+// greet : (Greeting, Name) -> PersonalizedGreeting
+
+// Example2.
+
+var greetWith = (Greeting gr) => (Name name) => $"{gr}, {name}";
+var greetFormally = greetWith("goodEvening");
+names.Map(greetFormally).ForEach(WriteLine);
+
+// greetWith : Greeting -> (Name- > PersonalizedGreeting)
+// arrow notaion 은 right associative 를 갖고 있어  Greeting -> Name -> PersonalizedGreeting 로 표기 가능하다.
+// greet with 같은걸 curried form 이라한다.
