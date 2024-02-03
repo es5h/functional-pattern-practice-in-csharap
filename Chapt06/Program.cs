930cee9f078eab25925c7aed868fd040c3152a4b
Eusneok Oh <lspk.lpg@gmail.com>
2024-02-04 06:18:02 +0900

    Chapt06 준비

--- Chapt06/Program.cs
+++ Chapt06/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using LanguageExt;
+
+public struct Age
+{
+  private int Value { get; }
+  
+  public static Option<Age> Create(int value) =>
+    value >= 0 && value <= 120
+      ? Option<Age>.Some(new Age(value))
+      : Option<Age>.None;
+  
+  private Age(int value) => Value = value;
+  
+  public static implicit operator int(Age age) => age.Value;
+  
+  private static bool IsValid(int age) => age >= 0 && age <= 120;
+}
---
+++ Chapt06/Readme.md
@@ -0,0 +1,15 @@
+﻿## Pattern?
+
+### 1. Fuctor
+- `C<T>` 는 Type T 를 가지는 Container 이다.
+- `Map : (C<T>, (T -> U)) -> C<U>`
+
+이러한 Map이 정의된 타입 C를 Functor라고 한다.
+IEnumerable 과 Option은 Functor의 예시이다.
+
+### 2. Bind
+- `Bind : (C<T>, (T -> C<U>)) -> C<U>`
+- 예시
+  - Int.Parse : `string -> Option<int>`
+  - Age.Create : `int -> Option<Age>`
+
