f18d183f7205d82cae60707840d6caf03dc2eb17
Eusneok Oh <lspk.lpg@gmail.com>
2024-02-05 00:05:56 +0900

    Chapt07 정리

--- Chapt07/Program.cs
+++ Chapt07/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LanguageExt;
+using Microsoft.AspNetCore.Mvc;
+using static LanguageExt.Option<MakeTransfer>;
+using static LanguageExt.Option<AccountState>;
 
-Console.WriteLine("Hello, World!");
+public class MakeTransferController(IValidator<MakeTransfer> validator, IRepository<AccountState> accounts, ISwiftService swiftService)
+{
+  public void MakeTransfer([FromBody] MakeTransfer transfer)
+    => Some(transfer)
+      .Map(Normallize)
+      .Filter(validator.IsValid)
+      .Iter(Book);
+
+  private MakeTransfer Normallize(MakeTransfer transfer) => transfer; // Temp Identity Map
+
+  private void Book(MakeTransfer transfer)
+  {
+    accounts.Get(transfer.From)
+      .Bind(from => from.Debit(transfer.Amount))
+      .Iter(account =>
+      {
+        accounts.Save(transfer.From, account);
+        swiftService.Send(transfer);
+      });
+  }
+}
+
+public interface ISwiftService
+{
+  void Send(MakeTransfer transfer);
+}
+
+public record AccountState(decimal Balance);
+
+public static class Account
+{
+  public static Option<AccountState> Debit(this AccountState from, decimal amount)
+    => from.Balance >= amount
+      ? Some(new AccountState(from.Balance - amount))
+      : Option<AccountState>.None;
+}
+
+public record MakeTransfer(Guid From, Guid To, decimal Amount);
+
+public interface IValidator<T>
+{
+  bool IsValid<T>(T t);
+}
+
+public interface IRepository<T>
+{
+  Option<T> Get(Guid id);
+  void Save(Guid id, T t);
+}
--- Chapt07/Readme.md
+++ Chapt07/Readme.md
@@ -1,1 +1,39 @@
-﻿- 
+﻿## Function Composition?
+
+### 1. C# 일반적인 함수의 합성
+- 예시
+
+```csharp
+Func<int, int> addOne = x => x + 1;
+Func<int, int> multiplyByTwo = x => x * 2;
+Func<int, int> addOneAndMultiplyByTwo = x => multiplyByTwo(addOne(x));
+```
+
+Method Chaning (with Extension Method)
+
+```csharp
+public static class IntExtensions
+{
+    public static int AddOne(this int x) => x + 1;
+    public static int MultiplyByTwo(this int x) => x * 2;
+}
+
+var result = 2.AddOne().MultiplyByTwo();
+```
+
+### 2. 고차 세계의 함수 합성
+- 예시
+```csharp
+Option<int> someOne = Some(1);
+Option<int> someNumber = someOne.Map(addOne).Map(multiplyByTwo); // Some(4)
+```
+- Functor Laws
+  - 1st law: for all Elements `ct` in `C<T>`, `ct.Map(x => x) == ct`
+  - 2nd law: for all Elements `ct` in `C<T>`, `ct.Map(f).Map(g) == ct.Map(x => g(f(x)))`
+  - [참고] 위의 두 법칙을 만족하는 것이 당연하게 들릴 수 있지만, Map 함수가 위의 두 법칙을 만족하지 않는다면, 그것은 Functor가 아니다.
+
+### 3. 합성이 잘되도록 만들기
+- **순수**하게: Function이 Side Effect가 없어야 한다. (Side Effect가 있다면 재사용되지 않는다.)
+- **Chaining**이 되도록: extension method를 사용하여 Method Chaining을 할 수 있도록 만들어야 한다.
+- **일반적**이게: 함수가 구체적일 수록, 재사용성이 떨어진다.
+- **형태를 유지**하도록: 함수의 형태를 유지하도록 만들어야 한다. Option은 Option으로, List는 List로, 등등.
