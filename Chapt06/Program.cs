cdb9d55292cbcf1d11a54bc7d5487f4412e5278b
Eusneok Oh <lspk.lpg@gmail.com>
2024-02-04 18:28:22 +0900

    Chapt6 예제 추가

--- Chapt06/Program.cs
+++ Chapt06/Program.cs
@@ -35,13 +35,58 @@ void Example2()
     new(Age.Create(121)),
   };
 
-  population.Map(x => x.Age).Iter(x => WriteLine(x.ToString())); // 42, None, 66, None
+  population.Map(x => x.Age).Iter(x => WriteLine(x.ToString())); // Some(42), None, Some(66), None
   population.Bind(x => x.Age).Iter(x => WriteLine(x.ToString())); // 42, 66
   WriteLine(population.Bind(x => x.Age).Map(x => x.Value).Average()); // 54
 }
 
-Example2();
+void Exercise1()
+{
+  Dictionary<string, int> dict = new()
+  {
+    ["one"] = 1,
+    ["two"] = 2,
+    ["three"] = 3,
+  };
+  
+  dict.Map(x => x + 1).Iter(x => WriteLine($"{x.Key} : {x.Value}")); // one : 2, two : 3, three : 4
+}
 
+void Exercise2()
+{
+  WriteLine(Option<int>.Some(42).Map2(x => x + 1)); // Some(43)
+  new List<int> { 1, 2, 3, 4, 5 }.Map2(x => x + 1).Iter(x => WriteLine(x.ToString())); // 2, 3, 4, 5, 6
+}
+
+// Exercise1();
+
+// Ex1. ISet, IDctionarary의 Map
+public static class DictionaryAndSetExt 
+{
+  public static IDictionary<K, R> Map<K, T, R>(this IDictionary<K, T> dict, Func<T, R> f) where K : notnull =>
+    dict.Select(kv => (kv.Key, f(kv.Value)))
+      .ToDictionary(kv => kv.Item1, kv => kv.Item2);
+  
+  public static ISet<R> Map<T, R>(this ISet<T> set, Func<T, R> f) =>
+    set.Select(f).ToHashSet();
+}
+
+
+// Ex2. Bind와 Return 으로 구현한 Map
+public static class EnumerableExt
+{
+  public static IEnumerable<R> Return<R>(R value) => new[] { value };
+    
+  public static IEnumerable<R> Map2<T, R>(this IEnumerable<T> ts, Func<T, R> f) =>
+    ts.Bind(t => Return(f(t)));
+}
+
+public static class OptionExt
+{
+  public static Option<T> Return<T> (T t) => Option<T>.Some(t);
+  public static Option<R> Map2<T, R>(this Option<T> opt, Func<T, R> f) =>
+    opt.Bind(t => Return(f(t)));
+}
 
 record Subject(Option<Age> Age);
 
